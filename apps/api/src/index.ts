d660154dd3adeae592d7edd78d6fe79e70170c82
viettienmilo <viettien.milo@gmail.com>
2026-06-03 19:23:57 +0700

    initial

---
+++ apps/api/src/index.ts
@@ -0,0 +1,39 @@
+import { serve } from '@hono/node-server';
+import { cors } from 'hono/cors';
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.use(
+  '*',
+  cors({
+    origin: 'http://localhost:5173',
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  }),
+);
+
+const routes = app.get('/api/hello', (c) => {
+  return c.json({
+    status: 'success',
+    message: 'Xin chào từ Hono Backend!',
+    timestamp: Date.now(),
+  });
+});
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+export type AppTypes = typeof routes;
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
